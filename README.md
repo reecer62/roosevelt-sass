4ef410d8dceb7bbd53b84380267f5b033d46216d
Troy Coutu <Autre31415@gmail.com>
2017-11-13 13:11:56 -0500

    initialize

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# roosevelt-sass
+roosevelt-sass
+===
+
+[SASS](http://sass-lang.com/) CSS preprocessor support for [Roosevelt MVC web framework](https://github.com/rooseveltframework/roosevelt).
+
+Usage
+===
+
+Declare this module as a dependency in your app, for example:
+
+```js
+"dependencies": {
+  "roosevelt": "*",
+  "roosevelt-sass": "*"
+}
+```
+
+Declare your CSS compiler by passing it as a param to Roosevelt:
+
+```js
+"rooseveltConfig": {
+  "cssCompiler": {nodeModule: "roosevelt-sass", params: {outputStyle: compressed}}
+}
+```
+
+See the [node-sass](https://github.com/sass/node-sass#options) repo API docs for documentation on available params.
---
+++ roosevelt-sass.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const path = require('path')
+const sass = require('node-sass')
+
+module.exports = {
+  versionCode: function (app) {
+    return `$${app.get('params').versionedCssFile.varName}: '${app.get('appVersion')}';\n`
+  },
+
+  parse: function (app, fileName) {
+    return new Promise((resolve, reject) => {
+      const params = app.get('params').cssCompiler.params
+      let options = params || {}
+
+      options.data = fs.readFileSync(path.join(app.get('cssPath'), fileName), 'utf8')
+      options.includePaths = [app.get('cssPath')]
+
+      // disable minify if noMinify param is present in roosevelt
+      if (app.get('params').noMinify) {
+        options.outputStyle = 'nested'
+      }
+
+      sass.render(options, (err, output) => {
+        if (err) {
+          reject(err)
+          return
+        }
+
+        let newCSS = output.css
+        let newFile = fileName.replace('.scss', '.css')
+
+        resolve([newFile, newCSS])
+      })
+    })
+  }
+}
